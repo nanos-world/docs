fbd9504fd205e93854eee4cd79b17de35773e3c1
MegaThorx <git@merx.dev>
2020-08-10 18:31:56 +0200

    Add new server-only, client-only and client-read-only icons

--- _static/js/script.js
+++ _static/js/script.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
 	$(".server-only-label").attr("title", "server-only");
 	$(".client-only-label").attr("title", "client-only");
 	$(".client-read-only-label").attr("title", "client-read-only");
+
+	$(".server-only-label").each(function(i, item) { AddClass(item.parentElement.parentElement, 'nanos-small-td-padding'); });
+	$(".client-only-label").each(function(i, item) { AddClass(item.parentElement.parentElement, 'nanos-small-td-padding'); });
+	$(".client-read-only-label").each(function(i, item) { AddClass(item.parentElement.parentElement, 'nanos-small-td-padding'); });
 });
 
 function DrawCard(url, image_url, header, body) {
@@ -13,3 +17,30 @@ function DrawCard(url, image_url, header, body) {
 		</a>
 	`);
 }
+
+// Add class helper
+function AddClass(elements, myClass) {
+
+	// if there are no elements, we're done
+	if (!elements) { return; }
+  
+	// if we have a selector, get the chosen elements
+	if (typeof(elements) === 'string') {
+	  elements = document.querySelectorAll(elements);
+	}
+  
+	// if we have a single DOM element, make it an array to simplify behavior
+	else if (elements.tagName) { elements=[elements]; }
+  
+	// add class to all chosen elements
+	for (var i=0; i<elements.length; i++) {
+  
+	  // if class is not already found
+	  if ( (' '+elements[i].className+' ').indexOf(' '+myClass+' ') < 0 ) {
+  
+		// add class
+		elements[i].className += ' ' + myClass;
+	  }
+	}
+  }
+  
--- scripting/common/common.rst
+++ scripting/common/common.rst
@@ -1,8 +1,8 @@
-.. |server-only-label| image:: /images/server_only.png
+.. |server-only-label| image:: /images/server-only.svg
    :class: server-only-label
 
-.. |client-only-label| image:: /images/server_only.png
+.. |client-only-label| image:: /images/client-only.svg
    :class: client-only-label
 
-.. |client-read-only-label| image:: /images/server_only.png
+.. |client-read-only-label| image:: /images/client-read-only.svg
    :class: client-read-only-label
