a2986911d49f364c2848a5b1126ee09e144d83d1
Gabriel T. Nardy <gtnardy@gmail.com>
2022-04-17 16:12:18 -0300

    Updated Vehicle to Chaos

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -126,7 +126,7 @@ module.exports = {
               label: 'bleeding-edge 🩸',
             },
             latest: {
-              label: 'latest - a1.5.0 ⚖️',
+              label: 'latest - a1.6.0 ⚖️',
             }
           },
         },
