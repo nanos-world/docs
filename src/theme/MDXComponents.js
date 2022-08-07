b0d0583580e4a945ed8891f9c5b1b331596f6645
Gabriel T. Nardy <gtnardy@gmail.com>
2022-08-07 17:02:06 -0300

    Updated Billboard, Decals, File, SceneCapture and StaticMesh to auto-docs; Added support for Static Functions; Added return types/descriptions to json

---
+++ src/theme/MDXComponents.js
@@ -0,0 +1,26 @@
+// Import the original mapper
+import MDXComponents from '@theme-original/MDXComponents';
+
+// Import custom elements
+import { HeaderDeclaration, ConstructorDeclaration, FunctionsDeclaration, StaticFunctionsDeclaration, EventsDeclaration } from '@site/docs/components/_class-builder.mdx';
+import { Structs, BasicType, AuthorityType, Classes, ReferenceLink, Enums, CardLink, AssetPath } from '@site/docs/components/_nanos.mdx';
+
+export default {
+	// Re-use the default mapping
+	...MDXComponents,
+
+	// Define custom elements
+	HeaderDeclaration,
+	ConstructorDeclaration,
+	FunctionsDeclaration,
+	StaticFunctionsDeclaration,
+	EventsDeclaration,
+	Structs,
+	BasicType,
+	AuthorityType,
+	Classes,
+	ReferenceLink,
+	Enums,
+	CardLink,
+	AssetPath
+};
