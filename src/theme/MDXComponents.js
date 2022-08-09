8813ec42ddc0f8140aa4ae35a53af0c02af62a18
Gabriel T. Nardy <gtnardy@gmail.com>
2022-08-09 07:47:00 -0300

    Another attempt to fix memory crash

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -2,8 +2,6 @@
 import MDXComponents from '@theme-original/MDXComponents';
 
 // Import custom elements
-import { HeaderDeclaration, ConstructorDeclaration, FunctionsDeclaration, StaticFunctionsDeclaration, EventsDeclaration } from '@site/src/components/ClassBuilder.mdx';
-import { EnumsDeclaration, EnumDeclaration } from '@site/src/components/EnumDeclaration.mdx';
 import { Structs, BasicType, AuthorityType, Classes, ReferenceLink, Enums, CardLink, AssetPath } from '@site/docs/components/_nanos.mdx';
 
 export default {
@@ -11,13 +9,6 @@ export default {
 	...MDXComponents,
 
 	// Define custom elements
-	HeaderDeclaration,
-	ConstructorDeclaration,
-	FunctionsDeclaration,
-	StaticFunctionsDeclaration,
-	EventsDeclaration,
-	EnumsDeclaration,
-	EnumDeclaration,
 	Structs,
 	BasicType,
 	AuthorityType,
