32731c3fb9656a577aba39dfa473b57d5366cf02
Gabriel T. Nardy <gtnardy@gmail.com>
2023-05-06 07:48:06 -0300

    Disabled API and versioned docs. Added Base Utility classes tooltips. Fixed reference links css. Attempt reenable crowdin.

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -4,7 +4,7 @@ import MDXComponents from '@theme-original/MDXComponents';
 // Import custom elements
 import { HeaderDeclaration, ConstructorDeclaration, FunctionsDeclaration, StaticFunctionsDeclaration, ExamplesDeclaration, EventsDeclaration, PropertiesDeclaration, APISourceURL } from '@site/src/components/ClassBuilder.mdx';
 import { EnumsDeclaration, EnumDeclaration } from '@site/src/components/EnumDeclaration';
-import { Structs, BasicType, AuthorityType, Classes, ReferenceLink, Enums, CardLink, AssetPath } from '@site/docs/components/_nanos.mdx';
+import { Structs, BasicType, AuthorityType, Classes, UtilityClasses, ReferenceLink, Enums, CardLink, AssetPath } from '@site/docs/components/_nanos.mdx';
 
 export default {
 	// Re-use the default mapping
@@ -21,6 +21,7 @@ export default {
 	EnumsDeclaration,
 	APISourceURL,
 	EnumDeclaration,
+	UtilityClasses,
 	Structs,
 	BasicType,
 	AuthorityType,
